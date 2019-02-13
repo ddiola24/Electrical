16aed4aded686843382513d8fc7877b8caaa7e36
ddiola24 <dianneyvonnemanalang@gmail.com>
2019-02-13 15:59:53 +0800

    logo

--- views/a_cus.php
+++ views/a_cus.php
@@ -59,7 +59,9 @@ $data =$db->getuser($_SESSION['username']);
         <div class="col-md-3 left_col">
           <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-              <a href="a_home.php" class="site_title"><img src="../vendors/img/favicon.png" width="50px" height="50px">   Electrical Shop</span></a>
+              <a href="a_home.php" class="site_title"><img src="../vendors/img/favicon.png" width="50px" height="50px">
+              <img src="../vendors/img/name.PNG" width="50px" height="50px">
+              </a>
             </div>
 
             <div class="clearfix"></div>
